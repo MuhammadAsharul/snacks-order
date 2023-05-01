e1b1e8174e7c1d85c48114c67b0566e80079681c
Muhammad Asharul <masharul51@gmail.com>
2023-05-01 11:47:21 +0700

    optional

--- resources/views/home/layouts/app.blade.php
+++ resources/views/home/layouts/app.blade.php
@@ -54,7 +54,7 @@
     <div class="container">
         <marquee behavior="" direction="left" style="background-color: #22223c;"><span
                 style="color:white;text-transform: uppercase">Pemesanan
-                Hanya Dilayani Untuk Wilayah Boyolali</span>
+                Hanya Dilayani Untuk Wilayah Boyolali || Harap melakukan pemesanan H-2</span>
         </marquee>
         @yield('content')
     </div>
